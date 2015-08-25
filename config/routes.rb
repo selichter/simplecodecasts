1a6255926438ec9bd9ddfdf685af0a9148fac3aa
selichter <lichter.sarahe@gmail.com>
2015-08-24 22:30:19 -0500

    Added contacts route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
     root 'pages#home'
     get '/about' => 'pages#about'
+    resources :contacts
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
